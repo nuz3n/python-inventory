f19ab9927bdcf7f86e6a7428ccd701ae302bd5de
nuz3n___ <nuz3n___@hotmail.com>
2020-04-27 19:13:52 +0700

    update code in stock

--- stock/views.py
+++ stock/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 from .models import Product
+
+# Create your views here.
 
 
 def index(request):
-    products = Product.object.all()
+    products = Product.objects.all()
+    # html = "<html><body>Home page </body></html>"
+    # return HttpResponse(html)
     return render(request, 'frontend/index.html', {'products': products})
 
 
